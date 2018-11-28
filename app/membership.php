7c43b57fb66a9dff3c35a7b41fe69d277532731c
Goran <gfvaskei@gmail.com>
2018-11-28 16:40:29 +0100

    Renamed style files, added template content to folder (ignored)

--- app/membership.php
+++ app/membership.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="styles/bootstrap.css">
+  <link rel="stylesheet" href="styles/bootstrap_solar.css">
 
   <!-- FontAwesome CSS -->
   <link rel="stylesheet" href="styles/fontawesome_all.css">
