3d501fbe5d6137d5b7aca4706e3e30752926b5bc
Goran <gfvaskei@gmail.com>
2019-06-11 22:58:42 +0200

    Update evipod DB

--- evipod.sql
+++ evipod.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 07, 2019 at 02:12 AM
+-- Generation Time: Jun 11, 2019 at 08:58 PM
 -- Server version: 5.7.21
 -- PHP Version: 7.2.10
 
@@ -45,17 +45,22 @@ CREATE TABLE IF NOT EXISTS `business` (
   `business_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `business_tel` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `business_mob` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`business_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `business`
 --
 
-INSERT INTO `business` (`business_id`, `business_name`, `user_id`, `business_owner`, `business_oib`, `business_mibpg`, `business_county`, `business_location`, `business_post`, `business_address`, `business_email`, `business_tel`, `business_mob`) VALUES
-(2, 'Test 1', 17, 'Djed Mraz', '00123456789', '123123', 'Međimurje', 'Doma&scaron;inec', '40318', 'Ulica 3', 'test@test.com', '', '098111111'),
-(3, 'Test 2', 17, 'Djed Mraz', '00123456789', '00123', 'Međimurje', 'Doma&scaron;inec', '40318', 'Ulica 3', 'test@test.com', '', '098111111'),
-(4, 'Test 3', 17, 'Djed Mraz', '', '', 'Međimurje', 'Doma&scaron;inec', '', '', '', '', '098111111');
+INSERT INTO `business` (`business_id`, `business_name`, `user_id`, `business_owner`, `business_oib`, `business_mibpg`, `business_county`, `business_location`, `business_post`, `business_address`, `business_email`, `business_tel`, `business_mob`, `created_at`, `updated_at`) VALUES
+(2, 'Test 1', 17, 'Djed Mraz', '00123456789', '123123', 'Međimurje', 'Doma&scaron;inec', '40318', 'Ulica 3', 'test@test.com', '', '098111111', '2019-06-07 15:17:22', '2019-06-07 15:17:22'),
+(3, 'Test 2', 17, 'Djed Mraz', '00123456789', '00123', 'Međimurje', 'Doma&scaron;inec', '40318', 'Ulica 3', 'test@test.com', '', '098111111', '2019-06-07 15:17:22', '2019-06-07 15:17:22'),
+(4, 'Test 3', 17, 'Djed Mraz', '', '', 'Međimurje', 'Doma&scaron;inec', '', '', '', '', '098111111', '2019-06-07 15:17:22', '2019-06-07 15:17:22'),
+(20, 'dfgdfgd45444gvhg', 17, '', '', '', '', '', '', '', '', '', '', '2019-06-07 16:19:16', '2019-06-07 16:19:16'),
+(21, 'dfgdfgd45444gvhg', 17, '', '', '', '', '', '', '', '', '', '', '2019-06-07 16:26:02', '2019-06-07 16:26:02'),
+(22, 'sfsdffse', 17, '', '', '', '', '', '', '', '', '', '', '2019-06-07 16:27:27', '2019-06-07 16:27:27');
 
 -- --------------------------------------------------------
 
@@ -99,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `is_email_confirmed`, `token_confirm`, `last_business_id`, `created_at`, `updated_at`) VALUES
-(17, 'Tester One', 'evipodtest1@gmail.com', '$2y$10$PtCaSRY2fIsG.iBc8ETA/.oeYvPOLFNwWpXX1sDhplMBY3xGC0VZ6', 1, '', NULL, '2018-12-29 00:33:40', '2019-06-03 00:40:39'),
+(17, 'Tester One', 'evipodtest1@gmail.com', '$2y$10$PtCaSRY2fIsG.iBc8ETA/.oeYvPOLFNwWpXX1sDhplMBY3xGC0VZ6', 1, '', 22, '2018-12-29 00:33:40', '2019-06-07 16:27:27'),
 (23, 'Žbulj', 'evipodtest3@gmail.com', '$2y$10$iD2Y6KQckevvGPCxlPZiCuYieJK/1WF.7pbpqMgZEP2fRpSTLtYY2', 1, '', NULL, '2019-03-02 23:28:47', '2019-03-02 23:29:23'),
 (30, 'Osoba', 'evipodtest2@gmail.com', '$2y$10$OONFIjH2iFVplQQ/TVUVIOuPRoUcsaE/4eaRtNXbiqH6si285l63.', 1, '', NULL, '2019-04-05 14:11:59', '2019-04-05 14:12:15');
 COMMIT;
